337119ff15c14dce482901bd9c0258e735663eda
Ishaq <isez9805@gmail.com>
2025-03-30 22:23:20 +0200

    update

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,4 +1,13 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
