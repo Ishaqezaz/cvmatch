6864084278fd7ede556ba3dc3cc9269134116567
Ishaq <isez9805@gmail.com>
2025-04-03 22:37:12 +0200

    fix: made cv and city required for cv scanning

--- src/api/Dtos/Profile/ProfileCreateDto.cs
+++ src/api/Dtos/Profile/ProfileCreateDto.cs
@@ -1,17 +1,19 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using api.Dtos.ProfileLocation;
 using api.Dtos.ProfileSkill;
+
 
 namespace api.Dtos.Profile
 {
     public class ProfileCreateDto
     {
+        [Required]
         public string? CVPath { get; set; }
-
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
         public string? City { get; set; }
-
-        public List<ProfileSkillCreateDto>? ProfileSkills { get; set; }
-
-        public List<ProfileLocCreateDto>? ProfileLocations { get; set; }
+        public List<ProfileSkillCreateDto>? Skills { get; set; } // derived
+        public List<ProfileLocCreateDto>? Locations { get; set; } // derived and calculated from city
     }
 }
