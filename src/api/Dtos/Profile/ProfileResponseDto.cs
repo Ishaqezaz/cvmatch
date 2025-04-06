d5d9c119ee0e229dae04ca9654e3ec776598f5a9
Ishaq <isez9805@gmail.com>
2025-04-06 22:27:22 +0200

    refactor: removed city field

--- src/api/Dtos/Profile/ProfileResponseDto.cs
+++ src/api/Dtos/Profile/ProfileResponseDto.cs
@@ -7,7 +7,6 @@ namespace api.Dtos.Profile
 {
     public class ProfileResponseDto
     {
-        public string City { get; set; } = string.Empty;
         public List<ProfileSkillResponseDto> Skills { get; set; } = new();
         public List<ProfileLocResponseDto> Locations { get; set; } = new();
     }
